87d449c4360d560bf9eb6ff0ba4a9fb7654d2ae1
darianbla <darian1vega@gmail.com>
2023-03-22 12:22:09 +0200

    pushing selling info and icon category start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
     <main class="app-container main-layout full">
         <section class="app-home main-layout full">
+
             <Hero />
 
             <section class="type-card">
@@ -34,15 +35,105 @@
 
                 </div>
             </section>
-            <div class="selling-proposition-warper">
+
+            <div class="selling-proposition-wrapper main-layout full">
                 <div class="selling-proposition">
                     <div class="selling-container">
                         <div class="selling-info">
-
+                            <h2>A whole world of freelance talent at your fingertips</h2>
+                            <ul>
+                                <li>
+                                    <section class="flex align-center">
+                                        <h6>
+                                            <span class="check-svg">
+                                                <svg width="16" height="16" viewBox="0 0 16 16"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z">
+                                                    </path>
+                                                    <path
+                                                        d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z">
+                                                    </path>
+                                                </svg>
+                                            </span>
+                                            <h3>The best for every budget</h3>
+                                        </h6>
+                                    </section>
+                                    <p>
+                                        Find high-quality services at every price point. No hourly rates, just project-based
+                                        pricing.
+                                    </p>
+                                </li>
+                                <li>
+                                    <section class="flex align-center">
+                                        <h6>
+                                            <span class="check-svg">
+                                                <svg width="16" height="16" viewBox="0 0 16 16"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z">
+                                                    </path>
+                                                    <path
+                                                        d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z">
+                                                    </path>
+                                                </svg>
+                                            </span>
+                                            <h3>Quality work done quickly</h3>
+                                        </h6>
+                                    </section>
+                                    <p>
+                                        Find the right freelancer to begin working on your project within minutes.
+                                    </p>
+                                </li>
+                                <li>
+                                    <section class="flex align-center">
+                                        <h6>
+                                            <span class="check-svg">
+                                                <svg width="16" height="16" viewBox="0 0 16 16"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z">
+                                                    </path>
+                                                    <path
+                                                        d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z">
+                                                    </path>
+                                                </svg>
+                                            </span>
+                                            <h3>Protected payments, every time</h3>
+                                        </h6>
+                                    </section>
+                                    <p>
+                                        Always know what you'll pay upfront. Your payment isn't released until you approve
+                                        the work.
+                                    </p>
+                                </li>
+                                <li>
+                                    <section class="flex align-center">
+                                        <h6>
+                                            <span class="check-svg">
+                                                <svg width="16" height="16" viewBox="0 0 16 16"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z">
+                                                    </path>
+                                                    <path
+                                                        d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z">
+                                                    </path>
+                                                </svg>
+                                            </span>
+                                            <h3>24/7 support</h3>
+                                        </h6>
+                                    </section>
+                                    <p>
+                                        Questions? Our round-the-clock support team is available to help anytime, anywhere.
+                                    </p>
+                                </li>
+                            </ul>
                         </div>
                         <div class="selling-video">
                             <div class="picture-wrapper">
-                                <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793711/gigs/ostlxosopzlu6ccomezg.webp" alt="">
+                                <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793711/gigs/ostlxosopzlu6ccomezg.webp"
+                                    alt="">
                                 <!-- <video src="src\assets\videos\vmvv3czyk2ifedefkau7.mp4"></video> -->
                             </div>
                         </div>
@@ -51,6 +142,44 @@
                 </div>
 
             </div>
+
+            <section class="icon-category">
+                <p class="bold"> Explore the marketplace</p>
+                <section class="category-list main-layout">
+                    <ul>
+                        <section class="icons">
+                            <span class="flex">
+                                <div>
+                                    <span>
+                                        <svg class="svg-graphic-design" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 64 64">
+                                            <defs>
+
+                                            </defs>
+                                            <g style="isolation:isolate">
+                                                <g id="white">
+                                                    <path fill="#fff" d="M18 30l2 21h-1v5h28v-5h-2l2-21H18z" />
+                                                    <path fill="#dee0e2" d="M47 30H18l.2 3H44l-2 19h2v4h3v-5h-2l2-21z" />
+                                                </g>
+                                                <g id="stroke">
+                                                    <path class="cls-4" d="M39.5 29.7L49.7 9.8l5.4 2.8-8.9 17.1" />
+                                                    <path class="cls-4"
+                                                        d="M51 29.2l5.3-10.4-8.9-4.6m-32.5 7.7l4.5 7.8m8.4 0l-6.7-11.8m-.1.9l-5.8 3.7m8.4 7.2l-4.9-9.2m17.8 9.2l.1-12.5-3.4-7.3-3.2 7.6v12.2m6.1-11.9l-6.1.3m3.3 11.6l-.2-10.8m12.5 31.9L47.3 30H18.2l1.6 20.8m-1.4 5.4h28.7v-5H18.4v5zM11.7 8.3a14.7 14.7 0 00-.2 8.9c1.5 4.1 5.1 5 7.9 1.7a2.4 2.4 0 00.7-2.4c-1.1-3.2-4.3-2.2-8.4-8.2z" />
+                                                </g>
+                                                <path
+                                                    d="M15 23l5 7h8l-6-11-7 4zM11.7 8.3a14.7 14.7 0 00-.2 8.9c1.5 4.1 5.1 5 7.9 1.7a2.4 2.4 0 00.7-2.4c-1.1-3.2-4.3-2.2-8.4-8.2zM39 30l11-20 5 3-9 17h-7zm-9 0h7l-1-12-3-8-3 8v12z"
+                                                    style="mix-blend-mode:darken" fill="#a1f4c0" id="flah" />
+                                            </g>
+                                        </svg>
+                                    </span>
+                                    <h4 class="regular">Graphics & Design</h4>
+                                </div>
+                            </span>
+
+                        </section>
+                    </ul>
+                </section>
+            </section>
         </section>
     </main>
 </template>
