c3f0bac023218201d1cd328ca16b3b749b1a2585
Eran Dolev <doleveran84@gmail.com>
2023-03-22 16:28:01 +0200

    long text

---
+++ src/components/LongText.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <h4>{{ displayTxt }}</h4>
+</template>
+
+<script>
+export default {
+    name: 'LongText',
+    props: {
+        txt: '',
+    },
+    data() {
+        return {
+            isShown: false,
+        }
+    },
+    computed: {
+        displayTxt() {
+            const length = 45
+            if (this.txt.length > 30)
+                return this.txt.slice(0, length) + '...'
+            return this.txt
+        }
+    }
+
+}
+</script>
