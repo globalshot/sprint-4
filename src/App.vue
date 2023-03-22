b06f8e4b0eab4f896310b0e2d836d7c846ca4a95
darianbla <darian1vega@gmail.com>
2023-03-22 19:31:24 +0200

    pulling gig detais

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,19 @@ import UserMsg from './components/UserMsg.vue'
 import Header from './components/AppHeader.vue'
 import Footer from './components/AppFooter.vue'
 
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
     name: 'App',
     components: {
         UserMsg,
         Header,
-        Footer
+        Footer,
+        VueperSlides,
+        VueperSlide,
     },
+
     created() {
         this.$store.dispatch({ type: 'loadStore' })
     }
