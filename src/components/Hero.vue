1bae6b341b7e44a8be0b2e186f06d3c38fe7a822
darianbla <darian1vega@gmail.com>
2023-03-22 10:08:57 +0200

    pulling before push hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,42 +1,50 @@
 <template>
     <section class="hero-container main-layout full">
-        <div class="img-container full">
-            <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp" alt="">
-        </div>
-        <div class="search-container flex">
-            <h1>
-                Find the perfect
-                <span class="fancy-word">freelance</span> <br>
-                services for your business
-            </h1>
-            <div class="hero-search flex">
-                <div class="input-container flex">
-                    <div class="search-icon flex">
-                        <span>
-                            <i class="fa-solid fa-magnifying-glass"></i>
-                        </span>
+        <div class="hero-pos-container main-layout full">
+
+            <div class="img-container full">
+                <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"
+                    alt="">
+            </div>
+            <div class="search-container flex">
+                <h1>
+                    Find the perfect
+                    <span class="fancy-word">freelance</span> <br>
+                    services for your business
+                </h1>
+
+                <div class="hero-search flex">
+                    <div class="input-container flex">
+                        <div class="search-icon flex">
+                            <span>
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </span>
+                        </div>
+                        <input type="text" name="" placeholder='Try "Logo Design"'>
                     </div>
-                    <input type="text" name="" placeholder='Try "Logo Design"'>
+                    <button class="el-button">
+                        <span>
+                            Search
+                        </span>
+                    </button>
                 </div>
-                <button class="el-button">
-                    <span>
-                        Search
-                    </span>
-                </button>
-            </div>
-            <div class="categories flex">
-                Popular:
-                <div class="category flex">website Design</div>
-                <div class="category flex">WordPress</div>
-                <div class="category flex">Logo Design</div>
-                <div class="category flex">AI Services</div>
+                <div class="categories flex">
+                    Popular:
+                    <div class="category flex">website Design</div>
+                    <div class="category flex">WordPress</div>
+                    <div class="category flex">Logo Design</div>
+                    <div class="category flex">AI Services</div>
+                </div>
+                <div class="hero-info flex">
+                    <p class="rate">
+                        <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
+                    </p>
+                    <p>Andrea, <span> Fashion Designer</span></p>
+                </div>
             </div>
         </div>
-        <div class="hero-info flex">
-            <p class="rate"><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i
-                    class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i></p>
-            <p>Andrea, <span> Fashion Designer</span></p>
-        </div>
+
     </section>
     <div class="trusted main-layout full">
         <div class="logos flex align-center justify-center">
@@ -59,11 +67,5 @@
         </div>
     </div>
     <section class="categories-list main-layout full">
-        <section class="type-card">
-            <div>
-                <h1>Popuar professional serfices</h1>
-                <div></div>
-            </div>
-        </section>
     </section>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,32 @@
     <main class="app-container main-layout full">
         <section class="app-home main-layout full">
             <Hero />
+            <section class="type-card">
+                <h1>Popuar professional serfices</h1>
+                <div class="gallery-container">
+                    <div class="subcategory-warper" style="background-image: url(https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/f27bec553efc12cc60baed89b8f2223e-1674661140708/ai-artists-2x.png) ;">
+                        <span>Add talent to AI</span>
+                        <h4>AI Artists</h4>
+                    </div>
+                    <div class="subcategory-warper" style="background-image: url(https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png) ;">
+                        <span>Build your brand</span>
+                        <h4>Logo Design</h4>
+                    </div>
+                    <div class="subcategory-warper" style="background-image: url(https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png) ;">
+                        <span>Customize your site</span>
+                        <h4>WordPress</h4>
+                    </div>
+                    <div class="subcategory-warper" style="background-image: url(https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png) ;">
+                        <span>Share your message</span>
+                        <h4>Voice Over</h4>
+                    </div>
+                    <div class="subcategory-warper" style="background-image: url(https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png) ;">
+                        <span>Engage your audience</span>
+                        <h4>Video Explainer</h4>
+                    </div>
+                   
+                </div>
+            </section>
         </section>
     </main>
 </template>
