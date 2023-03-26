141568cfc47d8103f1199d2e2323fe1cc2cdd13e
darianbla <darian1vega@gmail.com>
2023-03-26 10:51:56 +0300

    pulling fixed components in details page

--- src/views/GigDetails.vue
+++ src/views/GigDetails.vue
@@ -81,7 +81,7 @@
                                                     </ul>
                                                 </div>
                                             </span>
-                                            
+
                                             <!-- <span class="rating-count"></span> -->
                                         </span>
                                         <!--less bold color-->
@@ -185,108 +185,112 @@
                     </div>
 
 
-                    <h2 class="section-title about-this-seller">About The Seller</h2>
-                    <div class="profile-card">
-                        <div class="seller-card">
-                            <div class="profile-info flex">
-                                <div class="user-profile-image flex">
-                                    <label for="profile-image" class="img-label">
-                                        <img class="img-user" src="../assets/images/profile-pic.png" alt="">
-                                    </label>
-                                    <!--some kind of label, there is his photo, and over it the level-->
-                                </div>
-                                <div class="user-profile-label flex">
-                                    <div class="username-line flex">
-                                        <RouterLink to="/">{{ gig.owner.fullname }}</RouterLink>
-                                        <!--add route to his profile-->
-                                        <div class="status-indicator">
-                                            <span class="circle-icon">online/offline</span><!--todo this-->
-                                        </div>
-                                    </div>
-                                    <div class="one-liner-rating-wrapper flex">
-                                        <p class="one-liner">(his moto i guess)</p>
-                                        <div class="color-yellow">{{ gig.owner.rate }} (number of ratings)</div>
-                                        <!--v-for for the stars-->
-                                    </div>
-                                    <button class="btn-contact-me">contact me</button>
-                                </div>
-                            </div>
+                    <!-- <AboutSeller /> NOT WORKING -->
 
-                            <div class="stats-desc">
-                                <ul class="user-stats flex"><!--v-for loop, or hard coded-->
-                                </ul>
-                                <article class="seller-desc">
-                                    <div class="inner">text text text</div>
-                                    <button>read more</button><!--check when this is a thing-->
-                                </article>
-                                <div class="notable-clients">
-                                    <div class="notable-clients-about">
-                                        <div class="notable-clients-title flex">
-                                            <span>Among my clients &nbsp;</span><!--some <b> class-->
-                                        </div>
-                                        &nbsp;
-                                        <div>question mark svg</div><!--more info about this section-->
-                                        <span>NEW</span><!--pink new-->
+                    <div class="about-seller">
+                        <h2 class="section-title about-this-seller">About The Seller</h2>
+                        <div class="profile-card">
+                            <div class="seller-card">
+                                <div class="profile-info flex">
+                                    <div class="user-profile-image flex">
+                                        <label for="profile-image" class="img-label">
+                                            <img class="img-user" src="../assets/images/profile-pic.png" alt="">
+                                        </label>
+                                        <!--some kind of label, there is his photo, and over it the level-->
                                     </div>
-                                    <ul class="client-list double flex"><!--v-for loop-->
-
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <section class="portfolio-external-wrapper">
-                        <section class="porfilio-presence">
-                            <div class="presence-header flex">
-                                <RouterLink to="/">link to the porfilios</RouterLink>
-                                <div>the question mark svg</div>
-                                <span>the new svg(ping one)</span>
-                            </div>
-                            <ul class="grid"><!--idk if there grid class-->
-                                <!--first image is 2x2, other 4 are 1x1-->
-                            </ul>
-                            <RouterLink to="/">more projects</RouterLink><!--add link ofc-->
-                        </section>
-                    </section>
-
-                    <form class="gig-packages-form" action="/payments"><!--it say add method post-->
-                        <!--lots of inputs idk-->
-                        <div class="gig-packages-table">
-                            <h2 class="section-title">compare packages</h2>
-                            <table>
-                                <colgroup>
-                                    <col>
-                                    <col>
-                                    <col>
-                                    <col>
-                                </colgroup>
-                                <tbody>
-                                    <tr class="package-type"><!--do this x2 times-->
-                                        <th class="package-row-label">Package</th>
-                                        <th class="package-row-price">
-                                            <div class="price-wrapper flex">
-                                                <p class="price">number</p>
+                                    <div class="user-profile-label flex">
+                                        <div class="username-line flex">
+                                            <RouterLink to="/">{{ gig.owner.fullname }}</RouterLink>
+                                            <!--add route to his profile-->
+                                            <div class="status-indicator">
+                                                <span class="circle-icon">online/offline</span><!--todo this-->
                                             </div>
-                                            <p class="type">Basic</p><!--there was b element-->
-                                            <p class="title">his title</p>
-                                        </th>
-                                    </tr>
-                                    <tr class="description">
-                                        <td class="package-row-label"></td>
-                                        <td>bla bla lba</td><!--2 more times-->
-                                    </tr>
+                                        </div>
+                                        <div class="one-liner-rating-wrapper flex">
+                                            <p class="one-liner">(his moto i guess)</p>
+                                            <div class="color-yellow">{{ gig.owner.rate }} (number of ratings)</div>
+                                            <!--v-for for the stars-->
+                                        </div>
+                                        <button class="btn-contact-me">contact me</button>
+                                    </div>
+                                </div>
 
-                                    <tr></tr>
-                                    <!--v-for for sure, not even bothering to write all of the rows(bonuses of each thing)-->
+                                <div class="stats-desc">
+                                    <ul class="user-stats flex"><!--v-for loop, or hard coded-->
+                                    </ul>
+                                    <article class="seller-desc">
+                                        <div class="inner">text text text</div>
+                                        <button>read more</button><!--check when this is a thing-->
+                                    </article>
+                                    <div class="notable-clients">
+                                        <div class="notable-clients-about">
+                                            <div class="notable-clients-title flex">
+                                                <span>Among my clients &nbsp;</span><!--some <b> class-->
+                                            </div>
+                                            &nbsp;
+                                            <div>question mark svg</div><!--more info about this section-->
+                                            <span>NEW</span><!--pink new-->
+                                        </div>
+                                        <ul class="client-list double flex"><!--v-for loop-->
 
-                                    <tr class="delivery-time">bla bla, the 4 colums</tr>
-                                    <tr class="select-package">bla bla, the 4 colums, but with button</tr>
-                                </tbody>
-                            </table>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </form>
+                        <section class="portfolio-external-wrapper">
+                            <section class="porfilio-presence">
+                                <div class="presence-header flex">
+                                    <RouterLink to="/">link to the porfilios</RouterLink>
+                                    <div>the question mark svg</div>
+                                    <span>the new svg(ping one)</span>
+                                </div>
+                                <ul class="grid"><!--idk if there grid class-->
+                                    <!--first image is 2x2, other 4 are 1x1-->
+                                </ul>
+                                <RouterLink to="/">more projects</RouterLink><!--add link ofc-->
+                            </section>
+                        </section>
 
+                        <form class="gig-packages-form" action="/payments"><!--it say add method post-->
+                            <!--lots of inputs idk-->
+                            <div class="gig-packages-table">
+                                <h2 class="section-title">compare packages</h2>
+                                <table>
+                                    <colgroup>
+                                        <col>
+                                        <col>
+                                        <col>
+                                        <col>
+                                    </colgroup>
+                                    <tbody>
+                                        <tr class="package-type"><!--do this x2 times-->
+                                            <th class="package-row-label">Package</th>
+                                            <th class="package-row-price">
+                                                <div class="price-wrapper flex">
+                                                    <p class="price">number</p>
+                                                </div>
+                                                <p class="type">Basic</p><!--there was b element-->
+                                                <p class="title">his title</p>
+                                            </th>
+                                        </tr>
+                                        <tr class="description">
+                                            <td class="package-row-label"></td>
+                                            <td>bla bla lba</td><!--2 more times-->
+                                        </tr>
 
+                                        <tr></tr>
+                                        <!--v-for for sure, not even bothering to write all of the rows(bonuses of each thing)-->
+
+                                        <tr class="delivery-time">bla bla, the 4 colums</tr>
+                                        <tr class="select-package">bla bla, the 4 colums, but with button</tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </form>
+                    </div>
+
+                    
                     <div class="gig-page-review">
                         <ReviewPage :gig='gig' />
                         <div class="">Review</div><!--some classes-->
@@ -477,11 +481,14 @@
 
 <script>
 import { gigService } from './../services/gig.service'
+
 import SidebarContect from '../components/gigDetails/SidebarContect.vue'
 import ReviewPage from './ReviewPage.vue'
 import StickyHeader from '../components/gigDetails/StickyHeader.vue'
 import GigOverview from '../components/gigDetails/GigOverview.vue'
 import GigReview from '../components/gigDetails/GigReview.vue'
+import AboutSeller from '../components/gigDetails/AboutSeller.vue'
+
 import { VueperSlides, VueperSlide } from "vueperslides";
 import "vueperslides/dist/vueperslides.css";
 
@@ -524,8 +531,11 @@ export default {
         StickyHeader,
         GigOverview,
         GigReview,
+        AboutSeller,
+
         VueperSlides,
         VueperSlide,
+
 
     },
 
