ec56820b4eaeb24b683a096786b223907e43279e
Eran Dolev <doleveran84@gmail.com>
2023-03-26 13:06:40 +0300

    user page

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -1,24 +1,42 @@
 <template>
-    User Details
+    <h3>User Details</h3>
+    
     <h1>{{ user.fullname }}</h1>
+    <button>
+        <RouterLink to="/edit">Add Gig</RouterLink>
+    </button>
+    <GigIndex/>
+
 </template>
 
 <script>
+import GigList from '../components/GigList.vue'
+import GigIndex from '../views/GigIndex.vue'
 import { userService } from '../services/user.service'
     export default {
-        name: 'UserDetails',
-        data() {
+    name: "UserDetails",
+    data() {
         return {
             user: null,
-        }
+            gigs: []
+        };
     },
     async created() {
-        const { id } = this.$route.params
+        const { id } = this.$route.params;
         this.user = (id) ?
-        await userService.getById(id) :
-            console.log('Wrong User')
+            await userService.getById(id) :
+            console.log("Wrong User");
+        console.log(this.user);
 
-        console.log(this.user)
     },
-    }
+    computed: {
+        gigs() {
+            return this.$store.getters.gigs
+        },
+    },
+    components: {
+         GigList,
+         GigIndex
+        }
+}
 </script>
