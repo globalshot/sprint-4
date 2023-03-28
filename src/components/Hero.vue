6a32dbb893eda441d3aaccc5cc0c92b7209ed582
darianbla <darian1vega@gmail.com>
2023-03-28 18:57:23 +0300

    fixed hero and header

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,16 +3,15 @@
         <div class="hero-pos-container main-layout full">
 
             <div class="img-container full">
-                <vueper-slides autoplay class="no-shadow" fade>
+                <vueper-slides autoplay class="no-shadow" fade :arrows="false" :bullets="false" :pause-on-hover="false">
                     <vueper-slide v-for="(slide, i) in slides" :key="i" >
-                    <template #content>
-                        <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"
-                            alt="">
+                        <template #content>
+                            <img :src=img[i] alt="">
                         </template>
-                        </vueper-slide>
+                    </vueper-slide>
                 </vueper-slides>
-                <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"
-                    alt="">
+                <!-- <img src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"
+                    alt=""> -->
             </div>
 
 
@@ -62,13 +61,13 @@
                         <div class="category flex">AI Services</div>
                     </RouterLink>
                 </div>
-                <div class="hero-info flex">
+                <!-- <div class="hero-info flex">
                     <p class="rate">
                         <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>
                     </p>
                     <p>Andrea, <span> Fashion Designer</span></p>
-                </div>
+                </div> -->
             </div>
         </div>
 
@@ -100,24 +99,55 @@
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 export default {
     data() {
         return {
             filterBy: {
                 txt: '',
             },
+            img: ["https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/062d9604f20f64f4b45f8501aeea8212-1674662964518/bg-hero-6-900-x1.png",
+                "https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp",
+                "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/2413b8415dda9dbd7756d02cb87cd4b1-1599595203045/bg-hero-2-1792-x1.png",
+                "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/d14871e2d118f46db2c18ad882619ea8-1599835783966/bg-hero-3-1792-x1.png",
+                "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/93085acc959671e9e9e77f3ca8147f82-1599427734108/bg-hero-4-1792-x1.png",
+                "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049970/bg-hero-5-1792-x1.png",
+
+
+            ],
 
             slides: [
                 {
-                    // id: 'slide-1',
+                    // valentina,
+                    img: ("https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/062d9604f20f64f4b45f8501aeea8212-1674662964518/bg-hero-6-900-x1.png"),
+                },
+                {
+                    // andrea,
                     img: ("https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"),
                 },
                 {
-                    // id: 'slide-2',
-                    img: ("https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670768271/gigs/zj3dflxnixaytffgqpn9.webp"),
+                    // moon,
+                    img: ("https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/2413b8415dda9dbd7756d02cb87cd4b1-1599595203043/bg-hero-2-900-x1.png"),
+                },
+                {
+                    // ritika,
+                    img: ("https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/d14871e2d118f46db2c18ad882619ea8-1599835783973/bg-hero-3-900-x1.png"),
+                },
+                {
+                    // zach,
+                    img: ("https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049964/bg-hero-4-900-x1.png"),
+                },
+                {
+                    // gabriela,
+                    img: ("https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049961/bg-hero-5-900-x1.png"),
                 },
             ]
         }
+    },
+    components: {
+        VueperSlides,
+        VueperSlide
     },
     methods: {
         // setFilterBy() {
