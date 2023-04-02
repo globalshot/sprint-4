262c55bae11c64e79085ef2778dca2370c959124
Eran Dolev <doleveran84@gmail.com>
2023-04-02 12:44:59 +0300

    pull

--- src/components/GigPreview.vue
+++ src/components/GigPreview.vue
@@ -54,8 +54,8 @@
                 </path>
             </svg>
             <span>{{ gig.owner.rate }}</span>
-            <span v-if="gig.reviews" class="num">({{ gig.reviews.length }})</span>
-            <span v-else class="num">(0)</span>
+            <span v-if="gig.reviews" class="num"> ({{ gig.reviews.length }})</span>
+            <span v-else class="num"> (0)</span>
             <!-- <span class="num">({{ this.gigReviews(gig) }})</span> -->
         </div>
         <div class="price-container flex">
